e15d43ee7a0e9d429d3b3da0499b55766cd4d97b
박도희 <dohee.pa@gmail.com>
2021-03-28 16:54:11 +0900

    205. Testing Region Mapping

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -2,10 +2,19 @@ import { View } from './View';
 import { User, UserProps } from '../models/User';
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userForm: '.user-form'
+    }
+  }
+
   template(): string {
     return `
       <div>
-        <div class="user-show"></div>
+        <div class="user-show">
+          <h1></h1>
+        </div>
         <div class="user-form"></div>
       </div>
     `;
